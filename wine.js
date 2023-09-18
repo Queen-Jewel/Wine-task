f3a2d3944879cc92d9f2968f2b5fc868df0a409c
Jeweljeff <ketachi.jeff@gmial.com>
2023-09-18 01:33:57 +0100

    box1 almost done

--- wine.js
+++ wine.js
@@ -14,29 +14,55 @@ parentDiv.style.justifyContent = 'space-between';
 
 
 // Box first clone
-const num1 = clonedDiv.querySelector("#numa");
-const bot = clonedDiv.querySelector("#bot");
-const numN = clonedDiv.querySelector("#numn");
-const flag = clonedDiv.querySelector("#flag");
-const fran = clonedDiv.querySelector(".mde");
-const dollar = clonedDiv.querySelector("#dol");
+let num1 = clonedDiv.querySelector("#numa");
+let bot = clonedDiv.querySelector("#bot");
+let numN = clonedDiv.querySelector("#numn");
+let flag = clonedDiv.querySelector("#flag");
+let fran = clonedDiv.querySelector(".mde");
+let dollar = clonedDiv.querySelector("#dol");
 
 
 
 // modify first clone
 num1.textContent = "4.7"
-bot.src = "images/-merlot-di-toscana-igt-buccia-nera.jpg"
-numN.textContent ="MERLOT-DI-TOSCANA   0.75L"
+bot.src = "images/les hauts.webp"
+numN.textContent ="FRAPPATO   0.75L"
 flag.src = "flags/fr.svg"
 fran.textContent = "FR"
 dollar.textContent = "1200$"
 
 // modify img props
-bot.width = "100px";
+bot.width = "100&"
+
+
+// Box second clone
+let num2 = secondClone.querySelector("#numa");
+let bot2 = secondClone.querySelector("#bot");
+let numN2 = secondClone.querySelector("#numn");
+let flag2 = secondClone.querySelector("#flag");
+let fran2 = secondClone.querySelector(".mde");
+let dollar2 = secondClone.querySelector("#dol");
 
 
 
+// modify SECOND clone
+num2.textContent = "4.5"
+bot2.src = "images/case paolin cabernate.webp"
+numN2.textContent ="CASE PAOLIN   0.75L"
+flag2.src = "flags/fr.svg"
+fran2.textContent = "FR"
+dollar2.textContent = "450$"
 
+// SECOND BOX
+// const secondBox = parentDiv.createElement("div");
+// secondBox.innerHTML = "clonedDiv"
+
+
+
+// APPENDING CHILDREN
 parentDiv.appendChild(clonedDiv);
 parentDiv.appendChild(secondClone);
+
+originalDiv.appendChild(firstBox);
+originalDiv.appendChild(secondBox);
 
